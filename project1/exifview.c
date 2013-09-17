4ee5cbfd39be1334ecea43a3a857767e6a5e8239
Alex Vallejo <vallejo.alex@gmail.com>
2013-09-17 15:11:02 -0400

    Initial commit for part 2 of project 1, exif viewer

---
+++ project1/exifview.c
@@ -0,0 +1,11 @@
+//Created By: Alex Vallejo
+//Date: 9/17/2013
+//Project: Rock, Paper, Scissors and EXIF Viewer
+//Class: CS 447
+
+#include <stdio.h>
+
+int main(){
+  printf("Bats");
+  return (0);
+}
