7a074c6ba2d69032977615f7a8c54e4eae91b7ea
Alex Vallejo <vallejo.alex@gmail.com>
2013-09-29 16:59:45 -0400

    Add structs and validations for images

--- project1/exifview.c
+++ project1/exifview.c
@@ -5,7 +5,64 @@
 
 #include <stdio.h>
 
-int main(){
-  printf("Bats");
-  return (0);
+/* Struct for the image header */
+typedef struct{
+  unsigned short start_marker;
+  unsigned short app_marker;
+  unsigned short app_marker_length;
+  char exif[4];
+  unsigned short nul_marker;
+  char endinness[2];
+  unsigned short version;
+  unsigned int offset;
+} Header;
+
+/* Struct for tiff tags */
+typedef struct{
+  unsigned short id;
+  unsigned short type;
+  unsigned int num_items;
+  char value[4];
+  unsigned int offset;
+} Tag;
+
+int main(int argc, char *argv[]){
+
+  //Variable declerations
+  int count;
+
+  // Verify the program was executed correctly
+  if (argc < 2 || argc > 2){
+    printf("Usage: ./exifview img.jpg\nTry again!\n\n");
+    return 0; 
+  }
+
+  // Variable decleration
+  FILE * file;
+  Header head;
+
+  file = fopen(argv[1], "rb");
+  fread(&head, sizeof(head), 1, file);
+
+  printf("%s",head.endinness);
+  
+  //Verify the files are properly formatted!
+  if (head.app_marker != 0xE1FF){ //verify no app0 marker is present
+    printf("The file is improperly formatted!\nFile contains an APP0 marker.\n\n");
+    return 0;
+  }
+
+  if (0 != strcmp(head.endinness, "II*")){
+    printf("Sorry, but we do not support big endinness!");
+    return 0;
+  }
+
+  if (0 != strcmp(head.exif, "Exif")){
+    printf("Your fine does not contain EXIF data!");
+    return 0;
+  }
+
+  
+
+  printf("End of the program.");
 }
