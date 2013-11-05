0e05637371598725ea4bf963b270a75432c379cb
Alex Vallejo <vallejo.alex@gmail.com>
2013-11-05 14:48:36 -0500

    Add files for project3

---
+++ project3/mymalloc.c
@@ -0,0 +1,13 @@
+/* Alex Vallejo
+ * amv49
+ * 11/5/2013
+ * CS 449 Project 3
+ */
+
+void *my_next_fit_malloc(int size){
+
+}
+
+void my_free(int *ptr){
+
+}
---
+++ project3/unit_tests.c
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main(){
+
+  printf("Unit testing your custom malloc...\n");
+
+}
