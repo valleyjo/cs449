b498f3d53caacca57560a7bac88ec0e4b76c2f51
Alex Vallejo <vallejo.alex@gmail.com>
2013-11-05 21:14:14 -0500

    Add first naieve implementation of malloc. Only moves the break by enough to make the hep big enough for the requested size. Add first unit test for my_malloc

--- project3/mymalloc.c
+++ project3/mymalloc.c
@@ -1,11 +1,22 @@
-/* Alex Vallejo
- * amv49
- * 11/5/2013
+/* Name: Alex Vallejo
+ * Username: amv49
+ * Peoplesoft: 357811
+ * Date: 11/5/2013
  * CS 449 Project 3
  */
 
+#include <stdio.h>
+#include <unistd.h>
+
 void *my_next_fit_malloc(int size){
 
+  void *ptr;
+  ptr = sbrk(0);
+
+  if (sbrk(size) == (void*)-1)
+    return NULL;
+  else
+    return ptr;
 }
 
 void my_free(int *ptr){
--- project3/unit_tests.c
+++ project3/unit_tests.c
@@ -1,12 +1,19 @@
+/* Name: Alex Vallejo
+ * Username: amv49
+ * Peoplesoft: 357811
+ * Date: 11/5/2013
+ * CS 449 Project 3
+ */
 
 #include <stdio.h>
 #include <unistd.h>
 
 int main(){
+  int *adr;
 
   printf("Unit testing your custom malloc...\n");
-  int *adr = sbrk(0);
-  printf("Address location: %d", *adr);
+  adr = sbrk(0);
+  printf("Initial Address location: %d\n", adr);
 
-
+  return 0;
 }
