6b87de18ae3ad4c88c02bb5ac64c270497bf7a2d
Alex Vallejo <vallejo.alex@gmail.com>
2013-11-06 00:34:14 -0500

    broken stuff before refactor

--- project3/mymalloc.c
+++ project3/mymalloc.c
@@ -8,12 +8,67 @@
 #include <stdio.h>
 #include <unistd.h>
 
+typedef struct node_element *Node;
+
+struct node_element{
+  int size;
+  char free;
+  struct node_element *next;
+  struct node_element *prev;
+};
+
+Node first;
+Node next;
+Node last;
+void* base;
+
+Node extend_heap(int size){
+  Node n;
+
+  n = sbrk(0);
+  if (sbrk(sizeof(struct node_element) + size) == (void*)-1)
+    return NULL;
+  n->size = size;
+  n->free = 0;
+  n->next = NULL;
+  n->prev = last;
+
+  if (last)
+    last->next = n;
+
+  return n;
+}
+
+/* Run once when the list is first defined
+Node init_list(){
+
+  if (sbrk(sizeof(struct Node_s) == (void*)-1))
+      return NULL;
+  first->size = 0;
+  first->free = 1;
+  first->next = NULL;
+  first->prev = NULL;
+}*/
+
 void *my_next_fit_malloc(int size){
 
+  if (base){
+    last = base;
+    b = find_block(&last, size);
+  }
+
+  else (
+
+  if (first == 0){
+    last = NULL;
+    sbrk(sizeof(struct node_element) + size);
+  }
+  if (!n)
+    return(NULL);
   void *ptr;
   ptr = sbrk(0);
 
-  if (sbrk(size) == (void*)-1)
+  if (sbrk(size)== (void*)-1)
     return NULL;
   else
     return ptr;
@@ -22,3 +77,4 @@ void *my_next_fit_malloc(int size){
 void my_free(int *ptr){
 
 }
+
--- project3/unit_tests.c
+++ project3/unit_tests.c
@@ -7,13 +7,29 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include "mymalloc.c"
 
 int main(){
-  int *adr;
+  int *init_adr;
+  int *old_adr;
+  int *top_adr;
 
   printf("Unit testing your custom malloc...\n");
-  adr = sbrk(0);
-  printf("Initial Address location: %d\n", adr);
+  init_adr = sbrk(0);
+  printf("Initial Address location: %d\n\n", init_adr);
+
+  old_adr = my_next_fit_malloc(20);
+
+  if (*old_adr != *init_adr){
+    printf("ERROR. Expected initial address and return address\
+            after the first allocation to be equal");
+    printf("Initial address: %d", init_adr);
+    printf("Returned Address: %d", old_adr);
+  }
+
+  top_adr = sbrk(0);
+  printf("Address after allocation of 20 bytes: %d\n\n",\
+          top_adr);
 
   return 0;
 }
