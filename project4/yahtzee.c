50c96b3126fc6a42e63512a8125141b358a46f43
Alex Vallejo <vallejo.alex@gmail.com>
2013-11-17 23:47:11 -0500

    Add assign_categories method to handle user's die placement. Refactor to use NUM_DIE constant. Rafactor print message at the beging of the game loop

--- project4/yahtzee.c
+++ project4/yahtzee.c
@@ -12,6 +12,38 @@
 #define NUM_DIE 5
 #define DICE_ROLL rand() % 6 + 1;
 
+void assign_categories(int *rolls, int *upper, int *lower){
+  char input[20];
+  int category_selection;
+
+  printf("\nPlace dice into:\n");
+  printf("1) Upper Section\n");
+  printf("2) Lower Section\n");
+
+  printf("\nSelection: ");
+  fgets(input, sizeof(input), stdin);
+  category_selection = atoi(input);
+
+  // Upper categories
+  if (category_selection == 1){
+    printf("\nPlace dice into:\n");
+    printf("1) Ones\n");
+    printf("2) Twos\n");
+    printf("3) Threes\n");
+    printf("4) Fours\n");
+    printf("5) Fives\n");
+    printf("6) Sizes\n");
+
+    printf("\nSelection: ");
+    fgets(input, sizeof(input), stdin);
+    category_selection = atoi(input);
+  }
+
+  // Lower categories
+  else
+    printf("Lower");
+}
+
 void reroll(int rolls[]){
   char input[NUM_DIE * 2], *token[NUM_DIE * 2];
   int i, reroll[NUM_DIE], num_rerolls = 0;
@@ -52,26 +84,29 @@ void reroll(int rolls[]){
 
 int main(){
 
-  int turns, i, dice_to_reroll;
-  char pluralize_turn[15], input[20];
-  int rolls[5];
+  int turns, i, dice_to_reroll, rolls[NUM_DIE], upper[6], lower[7];
+  char turns_msg[30], input[20];
+
 
   srand(time(NULL));
 
-  for (turns = 13; turns > 0; turns--){
-    if (turns == 1)
-      strcpy(pluralize_turn, "turn");
-    else
-      strcpy(pluralize_turn, "turns");
+  for (turns = 13; turns >= 0; turns--){
 
-    printf("\nYou have %d %s left\n", turns, pluralize_turn);
+    if (turns == 0)
+      strcpy(turns_msg, "This is your last turn\n");
+
+    else if (turns == 1)
+      printf("You have 1 turn left\n");
+
+    else
+      printf("You have %d turns left\n", turns);
 
     // Roll the dice 5 times and print the result to the user
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < NUM_DIE; i++)
       rolls[i] = DICE_ROLL;
 
     printf("You rolled:\n");
-    for (i = 0; i < 5; i++)
+    for (i = 0; i < NUM_DIE; i++)
       printf("%d ", rolls[i]);
     printf("\n");
 
@@ -80,6 +115,8 @@ int main(){
 
     // Ask again according to game requirements
     reroll(rolls);
+
+    assign_categories(rolls, upper, lower);
   }
 
   return 0;
